06c77d91f7be02f669e2313b4193da7e1f649402
Benjamin Knudsen <benjaschou12@yahoo.dk>
2019-10-23 15:59:42 +0200

    Rettelser

--- contact.php
+++ contact.php
@@ -11,7 +11,6 @@ $headers = "From: ".$mailFrom;
 $txt = "You have received an e-mail from ".$name.".\n\n".$message;
 
 mail($mailTo, $subject, $txt, $headers);
-header("Location: index.html?mailsend");
 }
 
 ?>
